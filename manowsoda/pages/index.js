9b81a72adbc73b103cdcc80b54c310e7dc2a8ad5
Bio Teeradech <82809098+BioTeeradech@users.noreply.github.com>
2021-07-28 17:51:51 +0700

    initial2

--- manowsoda/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Head from 'next/head'
-
-export default function Home() {
-  return (
-    <h1 className="text-8xl text-green-600">Manow Soda</h1>
-  )
-}
---
+++ manowsoda/pages/index.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1 className="text-8xl text-green-600">sasa Soda</h1>
+  )
+}
