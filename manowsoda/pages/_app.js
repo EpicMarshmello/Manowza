06311e222a1e190ed521d2db66c7490e89ab63bf
BrownPharadol <brown01945@gmail.com>
2021-07-28 22:40:18 +0700

    menubar2

--- manowsoda/pages/_app.js
+++ manowsoda/pages/_app.js
@@ -1,7 +1,19 @@
+import React from "react";
+import Link from "next/link";
+import Nav from "../components/Nav";
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div>
+
+
+    <Nav/>
+    <Component {...pageProps} />
+
+
+  </div>
+  )
 }
 
 export default MyApp
---
+++ manowsoda/pages/cultural.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Cultural() {
+  return (
+    <div>
+      <h1>Caltural Maps</h1>
+    </div>
+  );
+}
