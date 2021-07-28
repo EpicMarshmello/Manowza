4f0fed9801819c691ed6d7bc7af17df70c261fae
Bio Teeradech <82809098+BioTeeradech@users.noreply.github.com>
2021-07-28 22:39:27 +0700

    New line

--- manowsoda/features/pages/landing_page.tsx
+++ manowsoda/features/pages/landing_page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export const LandingPage = () => {
-  return <div style={{color:"green"}} className="w-auto h-auto bg-pink-400">DaDazaza</div>;
+  return (
+    <>
+      <div style={{ color: "green" }} className="w-auto h-auto bg-pink-400">
+        DaDazaza
+      </div>
+      <div style={{backgroundImage: "/main.jpg')"}}></div>
+    </>
+  );
 };
