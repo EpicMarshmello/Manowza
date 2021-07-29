e61c0a9ac7c852139d4bebbc66923fe4fe18e7da
Bio Teeradech <82809098+EpicMarshmello@users.noreply.github.com>
2021-07-29 14:44:04 +0700

    add body

---
+++ manowsoda/features/components/bodyComponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const BodyItem = () => {
+  //---------------------------
+  //    RENDER
+  //---------------------------
+  return <div className=" text-9xl text-red-400 text-chonburi">ราชบุรี</div>;
+};
--- manowsoda/features/pages/landing_page.tsx
+++ manowsoda/features/pages/landing_page.tsx
@@ -1,10 +1,19 @@
+import { BodyItem } from "../components/bodyComponent";
 import React from "react";
+import Head from "next/head";
 
 export const LandingPage = () => {
+  //--------------------------
+  //  RENDER
+  //--------------------------
   return (
     <div
       style={{ backgroundImage: "url('/main.jpg')" }}
       className="w-screen h-screen bg-fixed"
-    ></div>
+    >
+      <div className="h-full w-full bg-black bg-opacity-10">
+        <BodyItem />
+      </div>
+    </div>
   );
 };
