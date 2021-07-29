525b99bf2f3d9e9b9893739b72587942747acdf6
Bio Teeradech <82809098+EpicMarshmello@users.noreply.github.com>
2021-07-29 13:29:38 +0700

    Update landing_page.tsx

--- manowsoda/features/pages/landing_page.tsx
+++ manowsoda/features/pages/landing_page.tsx
@@ -3,10 +3,12 @@ import React from "react";
 export const LandingPage = () => {
   return (
     <>
-      <div style={{ color: "green" }} className="w-auto h-auto bg-pink-400">
-        DaDazaza
+      <div style={{ color: "green" }} className="text-9xl bg-pink-400">
+        Daaaa
+        <div style={{ color: "red" }} className="text-7xl w-1/2 h-auto bg-blue-400">
+          DAaaaa
+        </div>
       </div>
-      <div style={{backgroundImage: "/main.jpg')"}}></div>
     </>
   );
 };
